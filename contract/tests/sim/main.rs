ddbf2eb23cd7d6b9d73dd8e91bb8fb91f98fbca7
Jovansonlee Cesar <ivanceras@gmail.com>
2022-04-04 17:46:30 +0800

    use the old name rust_counter_tutorial

--- contract/tests/sim/main.rs
+++ contract/tests/sim/main.rs
@@ -1,7 +1,7 @@
-use contract::CounterContract;
 pub use near_sdk::json_types::{Base64VecU8, ValidAccountId, WrappedDuration, U64};
 use near_sdk::serde_json::json;
 use near_sdk_sim::{call, deploy, init_simulator, view, ContractAccount, UserAccount};
+use rust_counter_tutorial::CounterContract;
 
 near_sdk_sim::lazy_static_include::lazy_static_include_bytes! {
     COUNTER_BYTES => "../out/main.wasm",
